f111448af4ac7d953f19158b6800279c15aeead3
Tim Wade <imtay.wade@gmail.com>
2013-10-19 18:15:34 -0400

    updated the sample code to be more comprehensible

--- sample.rb
+++ sample.rb
@@ -3,12 +3,16 @@ sheet = book.sheets.create('sheet 1',
                            a: {id: :data_1, name: 'a header'},
                            b: {id: :data_2, name: 'another header'},
                            c: {id: :data_3, name: 'yet another header'})
-sheet.name = new_sheet_name
+sheet.name = 'new sheet'
+
+# do some manipulation:
+
+# some header-generating code....
 sheet.headers = new_headers_array
-sheet.headers << new_headers
+sheet.headers << new_header
 sheet.headers.insert(:b, {id: data_4, name: 'inserted header'})
 
-# some data-generating code
+# some data-generating code....
 
 data.each { |d| sheet.insert(d) }
 
