ae62d1c924c2a4738f9122bcb3a2cb683552529a
Tim Wade <twade@peachtreebilling.com>
2013-10-21 15:07:57 -0400

    updated tests to pass now with new class method #build added to Book

--- spec/ted/book_spec.rb
+++ spec/ted/book_spec.rb
@@ -5,9 +5,13 @@ module Ted
     subject { Book.new('filename') }
 
     it { should respond_to(:filename) }
-    it { should respond_to(:build) }
     it { should respond_to(:sheets) }
     it { should respond_to(:save) }
+
+    describe ".build" do
+      specify { Book.should respond_to(:build) }
+      specify { Book.build('filename').should be_an_instance_of(Book) }
+    end
 
     describe "#sheets" do
       specify { subject.sheets.should be_an_instance_of(Ted::Sheets) }
