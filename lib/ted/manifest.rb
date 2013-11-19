0bb0c226cf7202d8e5799f66ae54b87ff8cdfaac
Tim Wade <imtay.wade@gmail.com>
2013-11-19 00:00:26 -0500

    finished manifest.rb

---
+++ lib/ted/manifest.rb
@@ -0,0 +1,84 @@
+module Ted
+  class Manifest < XmlDoc
+    def generate
+      markup.manifest(:manifest, manifest_attrs) do |mm|
+        private_methods(false).drop(1).each do |attrs_method|
+          mm.manifest(:'file-entry', send(attrs_method))
+        end
+      end
+    end
+
+  private
+
+    def manifest_attrs
+      {
+        'xmlns:manifest' => "urn:oasis:names:tc:opendocument:xmlns:manifest:1.0",
+        'manifest:version' => "1.2"
+      }
+    end
+
+    def base_path_attrs
+      {
+        'manifest:full-path' => "/",
+        'manifest:version' => "1.2",
+        'manifest:media-type' => "application/vnd.oasis.opendocument.spreadsheet"
+      }
+    end
+
+    def meta_path_attrs
+      {
+        'manifest:full-path' => "meta.xml",
+        'manifest:media-type' => "text/xml"
+      }
+    end
+
+    def settings_path_attrs
+      {
+        'manifest:full-path' => "settings.xml",
+        'manifest:media-type' => "text/xml"
+      }
+    end
+
+    def content_path_attrs
+      {
+        'manifest:full-path' => "content.xml",
+        'manifest:media-type' => "text/xml"
+      }
+    end
+
+    def thumbnails_path_attrs
+      {
+        'manifest:full-path' => "Thumbnails/thumbnail.png",
+        'manifest:media-type' => "image/png"
+      }
+    end
+
+    def manifest_path_attrs
+      {
+        'manifest:full-path' => "manifest.rdf",
+        'manifest:media-type' => "application/rdf+xml"
+      }
+    end
+
+    def accellerator_path_attrs
+      {
+        'manifest:full-path' => "Configurations2/accelerator/current.xml",
+        'manifest:media-type' => ""
+      }
+    end
+
+    def configuration_path_attrs
+      {
+        'manifest:full-path' => "Configurations2/",
+        'manifest:media-type' => "application/vnd.sun.xml.ui.configuration"
+      }
+    end
+
+    def styles_path_attrs
+      {
+        'manifest:full-path' => "styles.xml",
+        'manifest:media-type' => "text/xml"
+      }
+    end
+  end
+end
---
+++ spec/ted/manifest_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+module Ted
+  describe Manifest do
+    it { should respond_to(:markup) }
+    it { should respond_to(:generate) }
+
+    describe "#markup" do
+      specify { subject.markup.should be_a_kind_of(Builder::XmlMarkup) }
+    end
+
+    describe "#generate" do
+      let(:content) { subject.generate }
+
+      specify { content.should be_a_kind_of(String) }
+
+      describe "attributes" do
+        it "specifies manifest version 1.2" do
+          content.should =~ /manifest:version="1.2"/
+        end
+      end
+
+      specify { content.should =~ /<manifest:manifest/ }
+      specify { content.should =~ /xmlns:manifest=\"urn:oasis:names:tc:opendocument:xmlns:manifest:1.0\"/ }
+      specify { content.should =~ /<manifest:file-entry manifest:full-path=/ }
+    end
+  end
+end
