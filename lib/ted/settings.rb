278fb43644aa0d7f6fb2a948d09f77906f90c22f
Tim Wade <imtay.wade@gmail.com>
2013-11-19 23:56:48 -0500

    added document settings attrs

--- lib/ted/settings.rb
+++ lib/ted/settings.rb
@@ -1,7 +1,23 @@
 module Ted
   class Settings < XmlDoc
     def generate
-      markup
+      document_settings
+    end
+
+  private
+
+    def document_settings(&block)
+      markup.office(:'document-settings', document_settings_attrs, &block)
+    end
+
+    def document_settings_attrs
+      {
+        'xmlns:office'   => "urn:oasis:names:tc:opendocument:xmlns:office:1.0",
+        'xmlns:xlink'    => "http://www.w3.org/1999/xlink",
+        'xmlns:config'   => "urn:oasis:names:tc:opendocument:xmlns:config:1.0",
+        'xmlns:ooo'      => "http://openoffice.org/2004/office",
+        'office:version' => "1.2"
+      }
     end
   end
 end
--- spec/ted/settings_spec.rb
+++ spec/ted/settings_spec.rb
@@ -21,6 +21,11 @@ module Ted
       end
 
       specify { content.should =~ /<office:document-settings/ }
+      specify { content.should =~ /xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0"/ }
+      specify { content.should =~ /xmlns:xlink="http:\/\/www.w3.org\/1999\/xlink"/ }
+      specify { content.should =~ /xmlns:xlink="http:\/\/www.w3.org\/1999\/xlink"/ }
+      specify { content.should =~ /xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0"/ }
+      specify { content.should =~ /xmlns:ooo="http:\/\/openoffice.org\/2004\/office"/ }
     end
   end
 end
